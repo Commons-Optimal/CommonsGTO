99748d22c70ccd9d1ddb16a56c36b35b87991c3c
Cyphr <95357894+gmcyphr@users.noreply.github.com>
2026-08-22 21:20:48 +0200

    Scope legacy matcher styles to username routes

---
+++ app/[username]/layout.tsx
@@ -0,0 +1,9 @@
+import '../gto-v2.css';
+import '../gto-v3.css';
+import '../match.css';
+import '../match-home.css';
+import '../match-premium.css';
+
+export default function UsernameLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return children;
+}
